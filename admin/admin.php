2766e25c8d994dfc70525c6aff9647bc4fbee586
Ben Huson <ben@thewhiteroom.net>
2011-11-01 08:58:28 +0000

    Added support for hierarchical taxonomies and split code into classes.

--- admin/admin.php
+++ admin/admin.php
@@ -1,120 +1,126 @@
 <?php
 
-class CSSTheme_Admin {
+class AttachmentTaxSupp_Admin {
 	
 	/**
 	 * Configure Admin
 	 */
-	function CSSTheme_Admin() {
-		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
-		add_action( 'save_post', array( $this, 'save_post' ) );
+	function AttachmentTaxSupp_Admin() {
+		global $AttachmentTaxSupp;
+		add_filter( 'attachment_fields_to_edit', array( $this, 'attachment_fields_to_edit' ), null, 2 );
+		add_filter( 'attachment_fields_to_save', array( $this, 'attachment_fields_to_save' ), null, 2 );
+		wp_enqueue_script( 'media_taxonomies', plugins_url( dirname( $AttachmentTaxSupp->plugin_basename ) . '/admin/js/admin.js' ), array( 'jquery', 'suggest', 'post' ) );
 	}
 	
 	/**
-	 * Add Meta Boxes
+	 * Edit Image Form
 	 */
-	function add_meta_boxes() {
-		$themes = $this->theme_array();
-		if ( count( $themes ) == 0 ) 
-			return;
-		$post_types = $this->get_supported_post_types();
-		foreach ( $post_types as $post_type ) {
-			add_meta_box(
-				'css_theme',
-				apply_filters( 'css_theme_title', 'CSS Theme' ),
-				array( $this, 'css_theme_meta_box' ),
-				$post_type,
-				'side'
-			);
+	function attachment_fields_to_edit( $form_fields, $post ) {
+		foreach ( $form_fields as $key => $val ) {
+			if ( isset( $val['hierarchical'] ) && taxonomy_exists( $val['name'] ) ) {
+				$tax_name = esc_attr( $val['name'] );
+				$taxonomy = get_taxonomy( $val['name'] );
+				if ( $val['hierarchical'] == true) {
+					//$popular_ids = wp_popular_terms_checklist( $tax_name );
+					ob_start();
+					//wp_terms_checklist( $post->ID, array( 'taxonomy' => $tax_name, 'popular_cats' => $popular_ids ) );
+					wp_terms_checklist( $post->ID, array( 'taxonomy' => $tax_name ) );
+					$checklist = ob_get_contents();
+					ob_end_clean();
+			
+					$html = '<div class="inside">
+						<div id="taxonomy-' . $tax_name . '" class="categorydiv">
+							<ul id="' . $tax_name . '-tabs" class="category-tabs">
+								<li class="tabs" style="display: inline;"><a href="#' . $tax_name . '-all" tabindex="3" style="text-decoration:none;">' . $taxonomy->labels->all_items . '</a></li>
+								<!--<li class="hide-if-no-js" style="display: inline;"><a href="#' . $tax_name . '-pop" tabindex="3" style="text-decoration:none;">' . __( 'Most Used' ) . '</a></li>-->
+							</ul>
+							<div id="' . $tax_name . '-pop" class="tabs-panel" style="display: none;">
+								<ul id="' . $tax_name . 'checklist-pop" class="categorychecklist form-no-clear" >
+								</ul>
+							</div>
+							<div id="' . $tax_name . '-all" class="tabs-panel">
+								<input type="hidden" name="' . $tax_name . '[]" value="0" />
+								<ul id="' . $tax_name . 'checklist" class="list:' . $tax_name . ' categorychecklist form-no-clear">
+									' . $checklist . '
+								</ul>
+							</div>';
+					/*
+					if ( current_user_can( $taxonomy->cap->edit_terms ) ) :
+					$html .= '
+							<div id="category-adder" class="wp-hidden-children">
+								<h4>
+									<a id="category-add-toggle" href="#category-add" class="hide-if-no-js" tabindex="3">+ Add New Category</a>
+								</h4>
+								<p id="category-add" class="category-add wp-hidden-child">
+									<label class="screen-reader-text" for="newcategory">Add New Category</label>
+									<input type="text" name="newcategory" id="newcategory" class="form-required form-input-tip" value="New Category Name" tabindex="3" aria-required="true">
+									<label class="screen-reader-text" for="newcategory_parent">
+										Parent Category:</label>
+									<select name="newcategory_parent" id="newcategory_parent" class="postform" tabindex="3">
+										<option value="-1">— Parent Category —</option>
+										<option class="level-0" value="33">Temp</option>
+										<option class="level-0" value="34">Temp 2</option>
+										<option class="level-1" value="36">&nbsp;&nbsp;&nbsp;Temp 5</option>
+										<option class="level-0" value="35">Temp 3</option>
+										<option class="level-0" value="1">Uncategorized</option>
+									</select>
+									<input type="button" id="category-add-submit" class="add:categorychecklist:category-add button category-add-sumbit" value="Add New Category" tabindex="3">
+									<input type="hidden" id="_ajax_nonce-add-category" name="_ajax_nonce-add-category" value="092592c9e4">
+									<span id="category-ajax-response"></span>
+								</p>
+							</div>';
+					endif;
+					*/
+					$html .= '
+						</div>
+					</div>';
+					$form_fields[$key]['input'] = 'html';
+					$form_fields[$key]['html'] = $html;
+				} else {
+					$disabled = !current_user_can( $taxonomy->cap->assign_terms ) ? ' disabled="disabled"' : '';
+					$html = '<div class="inside">
+						<div class="tagsdiv" id="' . $tax_name . '">
+							<div class="jaxtag">
+								<div class="nojs-tags hide-if-js">
+									<p>' . $taxonomy->labels->add_or_remove_items . '</p>
+									<textarea name="attachments[' . $post->ID . '][' . $tax_name . ']" rows="3" cols="20" class="the-tags" id="attachments[' . $post->ID . '][' . $tax_name . ']"' . $disabled . ' style="margin:0px;">' . get_terms_to_edit( $post->ID, $tax_name ) . '</textarea>
+								</div>';
+					if ( current_user_can( $taxonomy->cap->assign_terms ) ) :
+						$html .= '<div class="ajaxtag hide-if-no-js">
+									<label class="screen-reader-text" for="new-tag-' . $tax_name . '">' . $val['labels']->name . '</label>
+									<div class="taghint screen-reader-text" style="">' . $taxonomy->labels->add_new_item . '</div>
+									<p><input type="text" id="new-tag-' . $tax_name . '" name="newtag[' . $tax_name . ']" class="newtag form-input-tip" size="16" autocomplete="off" value="">
+										<input type="button" class="button tagadd" value="' . esc_attr( 'Add' ) . '" tabindex="3"></p>
+								</div>
+								<p class="howto">' . esc_attr( $taxonomy->labels->separate_items_with_commas ) . '</p>
+							</div>';
+					endif;
+					$html .= '<div class="tagchecklist"></div>';
+					if ( current_user_can( $taxonomy->cap->assign_terms ) ) :
+						$html .= '<p class="hide-if-no-js"><a href="#titlediv" class="tagcloud-link" id="link-' . $tax_name . '">' . $taxonomy->labels->choose_from_most_used . '</a></p>';
+					endif;
+					$html .= '</div>';
+					$form_fields[$key]['input'] = 'html';
+					$form_fields[$key]['html'] = $html;
+				}
+			}
 		}
+		return $form_fields;
 	}
 	
 	/**
-	 * Save Property
-	 * Updates the property meta data when the post is saved.
+	 * Save Image Form
 	 */
-	function save_post( $post_id ) {
-		global $CSSTheme;
-		
-		// Verify if this is an auto save routine. 
-		// If it is our form has not been submitted, so we dont want to do anything
-		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) 
-			return;
-		
-		// Verify this came from the our screen and with proper authorization,
-		// because save_post can be triggered at other times
-		if ( !isset( $_POST['css_theme_noncename'] ) || !wp_verify_nonce( $_POST['css_theme_noncename'], $CSSTheme->plugin_basename ) )
-			return;
-		
-		// Check edit capability
-		$abort = true;
-		$post_types = $this->get_supported_post_types();
-		$post_types_obj = (array) get_post_types( array(
-			'public'   => true,
-			'_builtin' => false
-		), 'objects' );
-		if ( in_array( $_POST['post_type'], $post_types ) ) {
-			if ( 'page' == $_POST['post_type'] && current_user_can( 'edit_page', $post_id ) )
-				$abort = false;
-			elseif ( 'post' == $_POST['post_type'] && current_user_can( 'edit_post', $post_id ) )
-				$abort = false;
-			elseif ( current_user_can( $post_types_obj[$_POST['post_type']]->cap->edit_post, $post_id ) )
-				$abort = false;
+	function attachment_fields_to_save( $post, $attachment ) {
+		if ( isset( $_POST['tax_input'] ) && is_array( $_POST['tax_input'] ) ) {
+			foreach ( $_POST['tax_input'] as $tax => $val ) {
+				if ( taxonomy_exists( $tax ) ) {
+					$val = array_map( 'absint', $val );
+					wp_set_object_terms( $post['ID'], $val, $tax );
+				}
+			}
 		}
-		if ( $abort )
-			return $post_id;
-		
-		update_post_meta( $post_id, '_css_theme', $_POST['css_theme'] );
-		
-		return $_POST['css_theme'];
-	}
-	
-	/**
-	 * Product Details Meta Box
-	 */
-	function css_theme_meta_box( $post ) {
-		global $CSSTheme;
-		$themes = $this->theme_array();
-		$css_theme = get_post_meta( $post->ID, '_css_theme', true );
-		
-		$selected_css_theme = array();
-		foreach ( $themes as $key => $val ) {
-			$selected_css_theme[$val] = '';
-		}
-		$selected_css_theme[$css_theme] = ' selected="selected"';
-		
-		// Use nonce for verification
-		wp_nonce_field( $CSSTheme->plugin_basename, 'css_theme_noncename' );
-		
-		echo '<select name="css_theme" style="width:100%;">';
-		foreach ( $themes as $key => $val ) {
-			echo '<option value="' . $val . '"' . $selected_css_theme[$val] . '>' . $key . '</option>';
-		}
-		echo '</select>';
-	}
-	
-	function theme_array() {
-		$themes = array();
-		return apply_filters( 'css_theme_array', $themes );
-	}
-	
-	/**
-	 * Get Supported Post Types
-	 */
-	function get_supported_post_types() {
-		$args = array(
-			'public'   => true,
-			'_builtin' => false
-		);
-		$post_types = (array) get_post_types( $args );
-		$post_types[] = 'post';
-		$post_types[] = 'page';
-		$supported = array();
-		foreach ( $post_types as $post_type ) {
-			if ( post_type_supports( $post_type, 'css_theme' ) )
-				$supported[] = $post_type;
-		}
-		return $supported;
+		return $post;
 	}
 	
 }
--- attachment-taxonomy-support.php
+++ attachment-taxonomy-support.php
@@ -4,98 +4,88 @@
 Plugin Name: Attachment Taxonomy Support
 Plugin URI: https://github.com/benhuson/attachment-taxonomy-support
 Description: Improved taxonomy support for media and attachments.
-Version: 2.5.3
+Version: 2.5.4
 Author: Ben Huson
 Author URI: http://www.benhuson.co.uk/
 License: GPLv2 or later
 */
 
-add_action( 'init', 'attachmenttaxsupp_init', 0 );
-function attachmenttaxsupp_init() {
-
-	$labels = array(
-		'name'              => _x( 'Attachment Tags', 'taxonomy general name' ),
-		'singular_name'     => _x( 'Attachment Tag', 'taxonomy singular name' ),
-		'search_items'      =>  __( 'Search Attachment Tags' ),
-		'all_items'         => __( 'All Attachment Tags' ),
-		'parent_item'       => __( 'Parent Attachment Tag' ),
-		'parent_item_colon' => __( 'Parent Attachment Tag:' ),
-		'edit_item'         => __( 'Edit Attachment Tag' ), 
-		'update_item'       => __( 'Update Attachment Tag' ),
-		'add_new_item'      => __( 'Add New Attachment Tag' ),
-		'new_item_name'     => __( 'New Attachment Tag Name' ),
-		'menu_name'         => __( 'Attachment Tags' ),
-	);
-	register_taxonomy( 'attachment_tag', 'attachment', array(
-		'hierarchical' => false,
-		'labels'       => $labels,
-		'show_ui'      => true,
-		'query_var'    => true,
-		'rewrite'      => true,
-	) );
+class AttachmentTaxSupp {
 	
-	if ( is_admin() || ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
-		add_filter( 'attachment_fields_to_edit', 'attachmenttaxsupp_attachment_fields_to_edit', null, 2 );
-		//add_filter( 'attachment_fields_to_save', 'attachmenttaxsupp_attachment_fields_to_save', null, 2 );
-		wp_enqueue_script( 'media_taxonomies', plugins_url( '/attachment-taxonomy-support/admin/js/admin.js' ), array( 'jquery', 'suggest', 'post' ) );
+	var $plugin_dir = '';
+	var $plugin_basename = '';
+	var $admin = null;
+	
+	/**
+	 * Configure Plugin
+	 */
+	function AttachmentTaxSupp() {
+		$this->plugin_dir = dirname( __FILE__ );
+		$this->plugin_basename = plugin_basename( __FILE__ );
+		add_action( 'init', array( $this, 'init_plugin' ), 5 );
 	}
 	
-}
-
-
-
-/**
- * Edit Image Form
- */
-function attachmenttaxsupp_attachment_fields_to_edit( $form_fields, $post ) {
-	foreach ( $form_fields as $key => $val ) {
-		if ( isset( $val['hierarchical'] ) && taxonomy_exists( $val['name'] ) ) {
-			$tax_name = esc_attr( $val['name'] );
-			$taxonomy = get_taxonomy( $val['name'] );
-			$disabled = !current_user_can( $taxonomy->cap->assign_terms ) ? ' disabled="disabled"' : '';
-			$html = '<div class="inside">
-				<div class="tagsdiv" id="' . $tax_name . '">
-					<div class="jaxtag">
-						<div class="nojs-tags hide-if-js">
-							<p>' . $taxonomy->labels->add_or_remove_items . '</p>
-							<textarea name="attachments[' . $post->ID . '][' . $tax_name . ']" rows="3" cols="20" class="the-tags" id="attachments[' . $post->ID . '][' . $tax_name . ']"' . $disabled . ' style="margin:0px;">' . get_terms_to_edit( $post->ID, $tax_name ) . '</textarea>
-						</div>';
-			if ( current_user_can( $taxonomy->cap->assign_terms ) ) :
-				$html .= '<div class="ajaxtag hide-if-no-js">
-							<label class="screen-reader-text" for="new-tag-' . $tax_name . '">' . $val['labels']->name . '</label>
-							<div class="taghint screen-reader-text" style="">' . $taxonomy->labels->add_new_item . '</div>
-							<p><input type="text" id="new-tag-' . $tax_name . '" name="newtag[' . $tax_name . ']" class="newtag form-input-tip" size="16" autocomplete="off" value="">
-								<input type="button" class="button tagadd" value="' . esc_attr( 'Add' ) . '" tabindex="3"></p>
-						</div>
-						<p class="howto">' . esc_attr( $taxonomy->labels->separate_items_with_commas ) . '</p>
-					</div>';
-			endif;
-			$html .= '<div class="tagchecklist"></div>';
-			if ( current_user_can( $taxonomy->cap->assign_terms ) ) :
-				$html .= '<p class="hide-if-no-js"><a href="#titlediv" class="tagcloud-link" id="link-' . $tax_name . '">' . $taxonomy->labels->choose_from_most_used . '</a></p>';
-			endif;
-			$html .= '</div>';
-			$form_fields[$key]['input'] = 'html';
-			$form_fields[$key]['html'] = $html;
+	/**
+	 * Configure Plugin
+	 */
+	function init_plugin() {
+		$this->setup_taxonomies();
+		if ( is_admin() || ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
+			require_once( $this->plugin_dir . '/admin/admin.php' );
+			$this->admin = new AttachmentTaxSupp_Admin();
 		}
 	}
-	return $form_fields;
-}
-
-/**
- * Save Image Form
- */
-function attachmenttaxsupp_attachment_fields_to_save( $post, $attachment ) {
-	/*
-	if ( isset( $attachment['cithumb'] ) ) {
-		$cithumb = absint( trim( $attachment['cithumb'] ) );
-		if ( $cithumb == 0 )
-			$cithumb = '';
-		update_post_meta( $post['ID'], '_cithumb', $cithumb );
+	
+	/**
+	 * Setup Taxonomies
+	 * Creates 'attachment_tag' and 'attachment_category' taxonomies.
+	 */
+	function setup_taxonomies() {
+		$labels = array(
+			'name'              => _x( 'Attachment Tags', 'taxonomy general name' ),
+			'singular_name'     => _x( 'Attachment Tag', 'taxonomy singular name' ),
+			'search_items'      =>  __( 'Search Attachment Tags' ),
+			'all_items'         => __( 'All Attachment Tags' ),
+			'parent_item'       => __( 'Parent Attachment Tag' ),
+			'parent_item_colon' => __( 'Parent Attachment Tag:' ),
+			'edit_item'         => __( 'Edit Attachment Tag' ), 
+			'update_item'       => __( 'Update Attachment Tag' ),
+			'add_new_item'      => __( 'Add New Attachment Tag' ),
+			'new_item_name'     => __( 'New Attachment Tag Name' ),
+			'menu_name'         => __( 'Attachment Tags' ),
+		);
+		register_taxonomy( 'attachment_tag', 'attachment', array(
+			'hierarchical' => false,
+			'labels'       => $labels,
+			'show_ui'      => true,
+			'query_var'    => true,
+			'rewrite'      => true,
+		) );
+		$labels = array(
+			'name'              => _x( 'Attachment Categories', 'taxonomy general name' ),
+			'singular_name'     => _x( 'Attachment Category', 'taxonomy singular name' ),
+			'search_items'      =>  __( 'Search Attachment Categories' ),
+			'all_items'         => __( 'All Attachment Categories' ),
+			'parent_item'       => __( 'Parent Attachment Category' ),
+			'parent_item_colon' => __( 'Parent Attachment Category:' ),
+			'edit_item'         => __( 'Edit Attachment Category' ), 
+			'update_item'       => __( 'Update Attachment Category' ),
+			'add_new_item'      => __( 'Add New Attachment Category' ),
+			'new_item_name'     => __( 'New Attachment Category Name' ),
+			'menu_name'         => __( 'Attachment Category' ),
+		);
+		register_taxonomy( 'attachment_category', 'attachment', array(
+			'hierarchical' => true,
+			'labels'       => $labels,
+			'show_ui'      => true,
+			'query_var'    => true,
+			'rewrite'      => true,
+		) );
 	}
-	*/
-	return $post;
+	
 }
 
+global $AttachmentTaxSupp;
+$AttachmentTaxSupp = new AttachmentTaxSupp();
 		
 ?>

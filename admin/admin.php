4bcd27d4436592d36b44a760300d10c4c659b3c5
Ben Huson <ben@thewhiteroom.net>
2012-05-23 23:24:23 +0100

    Remove $current_screen->id debug. Oops.

--- admin/admin.php
+++ admin/admin.php
@@ -57,7 +57,6 @@ class AttachmentTaxSupp_Admin {
 	 */
 	function admin_enqueue_scripts() {
 		global $AttachmentTaxSupp, $current_screen;
-		echo  $current_screen->id;
 		if ( ( $current_screen->id == 'media' ) || ( isset( $_GET['taxonomy'] ) && isset( $_GET['post_type'] ) && 'attachment' == $_GET['post_type'] ) ) {
 			wp_enqueue_script( 'media_taxonomies', plugins_url( dirname( $AttachmentTaxSupp->plugin_basename ) . '/admin/js/admin.js' ), array( 'jquery', 'suggest', 'post' ) );
 		}
